2b7d012a93267374a77f4ae244fb3318c8400b54
Moritz Biehl <biehlmo@gmail.com>
2024-07-09 14:01:49 +0200

    Display List of all Art Pieces

---
+++ components/ArtPiecePreview.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import styled from "styled-components";
+
+export default function ArtPiecePreview({ image, title, artist }) {
+  console.log(image);
+  return (
+    <StyledListItem>
+      <Image src={image} alt="Art Picture" width={300} height={300} />
+      <p>{`"${title}" by ${artist}`}</p>
+    </StyledListItem>
+  );
+}
+
+const StyledListItem = styled.li`
+  list-style: none;
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
+import useSWR from "swr";
+import ArtPieces from "@/components/ArtPieces";
+
 export default function HomePage() {
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const URL = "https://example-apis.vercel.app/api/art";
+
+  const { data, error, isLoading } = useSWR(URL, fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>ART GALLERY</h1>
+      <ArtPieces pieces={data} />
     </div>
   );
 }
